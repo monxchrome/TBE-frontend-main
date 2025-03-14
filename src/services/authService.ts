7dd65f3d80a63696ef7a31cc6cec9cd4f8ede128
monxchrome <inacheat@gmail.com>
2025-03-14 17:32:50 +0100

    Added auth service

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -24,7 +24,7 @@ export const authService = {
 		});
 
 		if (!response.ok) {
-			throw new Error('Ошибка обновления токена');
+			throw new Error('Refresh token error');
 		}
 
 		const result = await response.json();
